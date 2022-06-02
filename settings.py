9faf2dbc6ba92d7c07c028c83a7838688713a3de
John Mark <john.mark@kistler.com>
2022-06-01 21:20:49 -0400

    Rearrange some values to allow override from settings.json

--- settings.py
+++ settings.py
@@ -1,11 +1,21 @@
 import json
 
+def SECONDS(seconds):
+    return seconds
+
+def MINUTES(seconds):
+    return 60 * SECONDS(seconds)
+
+def HOURS(seconds):
+    return 60 * MINUTES(seconds)
+
 CLOCK_RADIUS = 270
 OWM_BASEURI = "https://api.openweathermap.org/data/2.5/onecall"
 OWM_EXCLUDES = "minutely,flags"
 
 ICON_SET_NAME = "owm-std"
-UPDATE_PERIOD = 10 * 60
+UPDATE_PERIOD = MINUTES(10)
+FETCH_DELAY_INCR = SECONDS(10)
 FONT_SIZE = 18
 
 def get_settings():
@@ -18,7 +28,10 @@ settings = get_settings()
 if "icon-set-name" in settings:
     ICON_SET_NAME = settings["icon-set-name"]
 
-if "update-period" in settings:
-    UPDATE_PERIOD = settings["update-period"]
+if "update-mins" in settings:
+    UPDATE_PERIOD = MINUTES(settings["update-mins"])
+
+if "update-retry-sec" in settings:
+    FETCH_DELAY_INCR = SECONDS(settings["update-retry-sec"])
 
 WEATHER_DATA_URL = f'{OWM_BASEURI}?lat={settings["latitude"]}&lon={settings["longitude"]}&exclude={OWM_EXCLUDES}&appid={settings["APIKEY"]}&units=metric'
